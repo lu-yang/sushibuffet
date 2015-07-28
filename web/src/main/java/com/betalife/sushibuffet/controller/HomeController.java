dfa40454058b36319343dbd0d847096ee1bed58d
Kevin Yang Lu <luy@shinetechchina.com>
2015-07-28 11:38:22 +0800

    fixed untracked files

--- web/src/main/java/com/betalife/sushibuffet/controller/HomeController.java
+++ web/src/main/java/com/betalife/sushibuffet/controller/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.betalife.sushibuffet.exchange.BaseExchange;
 import com.betalife.sushibuffet.exchange.BooleanExchange;
---
+++ web/src/main/java/com/betalife/sushibuffet/util/PrinterAdapter.java
@@ -0,0 +1,25 @@
+package com.betalife.sushibuffet.util;
+
+import java.util.List;
+
+public class PrinterAdapter implements Printer {
+
+	@Override
+	public void print(List<?> lines, boolean logo, int times) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLogo(String logo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getCutPaper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
